0d86d6ac23456c86d121c4f5d155ee9c6752d480
Zhipeng <mrzpding@gmail.com>
2023-02-14 21:04:22 -0600

    add controllers and views for pokemons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "pokemons/index"
+  get "pokemons/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "/pokemons", to: "pokemons#index", as: "pokemons"
+  # get "/pokemons/:id", to: "pokemons#show", as: "pokemon"
 end

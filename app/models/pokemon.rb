5378c39b72da4ecbb4587f95bbd38c67fe6eddc2
Zhipeng <mrzpding@gmail.com>
2023-03-10 00:08:14 -0600

    improve the hierarchical collection navigation

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon < ApplicationRecord
   belongs_to :type
   has_many :player_pokemons
-  has_many :pokemons, through: :player_pokemons
+  has_many :players, through: :player_pokemons
   paginates_per 20
 end
